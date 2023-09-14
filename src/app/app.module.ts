d3d693a54e9059e203e6bc7ed8bf39332704b7e9
Mar Ndiaye <mar.ndiaye221@gmail.com>
2023-09-14 15:45:21 +0000

    TailwindCssDesign: Création de la topbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SideBarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
